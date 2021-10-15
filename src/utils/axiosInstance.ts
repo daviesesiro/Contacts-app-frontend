a9f0de1daecc7ebf8244d7f1a8563b3bd789d162
daviesesiro <ojurereoluwad@gmail.com>
2021-10-15 03:08:24 +0100

    updated prod api url

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -3,7 +3,7 @@ import Axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:9000/api"
-    : "https://localhost:9000/api";
+    : "https://icanblog.herokuapp.com/api";
 
 const axios = Axios.create({ baseURL });
 
